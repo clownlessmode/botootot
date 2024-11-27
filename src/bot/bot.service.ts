900e84d1cb609b12f0b8722c7d3dbc69a46da37a
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2024-11-27 21:24:27 +0700

    fix

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -27,13 +27,13 @@ export class BotService {
         [
           Markup.button.webApp(
             'Открыть визитку 🌐',
-            'https://r6nt2plp-8080.asse.devtunnels.ms/%D0%A0%D0%B5%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D1%83%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F%20%D0%BA%D1%80%D0%B5%D0%B4%D0%B8%D1%82%D0%BE%D0%B2.html'
+            'https://khal-web-app.vercel.app/'
           ),
         ],
       ]),
     });
 
-    const videoPath = path.join(__dirname, './../../../video.mp4');
+    const videoPath = path.join(__dirname, './../../video.mp4');
 
     // Отправляем видео заметку с явным указанием типа
     await ctx.telegram.sendVideoNote(
